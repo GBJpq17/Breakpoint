5a4ccad3de6d1e8899e46a440952cfb4132b67c7
Ryan Loera <rloera@csumb.edu>
2017-09-22 16:16:01 -0400

    added CreatePostVC with function to post message to Firebase

---
+++ Breakpoint/Controller/CreatePostVC.swift
@@ -0,0 +1,48 @@
+//
+//  CreatePostVC.swift
+//  Breakpoint
+//
+//  Created by RyLo on 9/22/17.
+//  Copyright © 2017 GBJpq. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class CreatePostVC: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textView.delegate = self
+    }
+    
+    @IBAction func sendButtonPressed(_ sender: Any) {
+        if textView.text != nil && textView.text != "Say something here..."{
+            sendButton.isEnabled = false
+            DataService.instance.uploadPost(withMessage: textView.text, forUID: (Auth.auth().currentUser?.uid)!, withGroupKey: nil, sendComplete: { (isComplete) in
+                if isComplete {
+                    self.sendButton.isEnabled = true
+                    self.dismiss(animated: true, completion: nil)
+                } else {
+                    self.sendButton.isEnabled = false
+                    print("There was an error")
+                }
+            })
+        }
+    }
+    
+    @IBAction func closeButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+extension CreatePostVC: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.text = ""
+    }
+}
