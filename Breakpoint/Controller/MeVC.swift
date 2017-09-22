b0f735e5a5bc96cdf641077c105affcaa44a08af
Ryan Loera <rloera@csumb.edu>
2017-09-22 09:25:20 -0400

    added MeVC to display profile of user

---
+++ Breakpoint/Controller/MeVC.swift
@@ -0,0 +1,24 @@
+//
+//  MeVC.swift
+//  Breakpoint
+//
+//  Created by RyLo on 9/22/17.
+//  Copyright © 2017 GBJpq. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    @IBAction func signOutPressed(_ sender: Any) {
+        
+    }
+    
+}
